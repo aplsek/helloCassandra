8359c708f11af6dc505d21d6fa508303377d2cf7
aplsek <aplsek@gmail.com>
2014-09-29 18:08:57 -0700

    helloworld example complete

--- src/main/java/hello/CassandraTest.java
+++ src/main/java/hello/CassandraTest.java
@@ -23,6 +23,23 @@ public class CassandraTest {
 					System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age")); 
 				}	
 
+				// Update the same user with a new age
+				session.execute("update users set age = 36 where lastname = 'Jones'");
+				// Select and show the change
+				results = session.execute("select * from users where lastname='Jones'");
+				for (Row row : results) {
+					System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+ 
+				}
+
+				// Delete the user from the users table
+				session.execute("DELETE FROM users WHERE lastname = 'Jones'");
+				// Show that the user is gone
+				results = session.execute("SELECT * FROM users");
+				for (Row row : results) {
+					System.out.format("%s %d %s %s %s\n", row.getString("lastname"), row.getInt("age"),  row.getString("city"), row.getString("email"), row.getString("firstname"));
+				}	
+
 				// Clean up the connection by closing it
 				cluster.close();
          }
