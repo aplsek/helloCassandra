112c8678675fad224c81fe8f2ef2d74b2271983a
aplsek <aplsek@gmail.com>
2014-09-29 17:54:11 -0700

    driver dependency, first queries

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Cassandra experiments.
 
 
 Based on http://planetcassandra.org/getting-started-with-apache-cassandra-and-java/
+
+
+
+References:
+
+https://github.com/otaviojava/Easy-Cassandra-samples
--- src/main/java/hello/CassandraTest.java
+++ src/main/java/hello/CassandraTest.java
@@ -1,10 +1,30 @@
 package hello;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.*;
+
 public class CassandraTest {
  
          public static void main(String[] args) {
+         		System.out.println("Hello Cassandra");
 
+         		// Connect to the cluster and keyspace "demo"
+				Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+				Session session = cluster.connect("demo");
 
+				// Insert one record into the users table
+				session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+
+				// Update the same user with a new age
+				session.execute("update users set age = 36 where lastname = 'Jones'");
+				// Select and show the change
+				ResultSet results = session.execute("select * from users where lastname='Jones'");
+				for (Row row : results) {
+					System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age")); 
+				}	
+
+				// Clean up the connection by closing it
+				cluster.close();
          }
 
 
